f921e54d4ba0c50f3be95df5b29734b2465a5144
Saulo Falcão - Casa <saulofalcao@gmail.com>
2015-12-02 12:33:05 -0300

    Comecando a fazer o helper pra mostrar as linhas

---
+++ client/wallpapersList/wallpaperList.controller.js
@@ -0,0 +1,17 @@
+Template.content.helpers({
+    wallpapers: function () {
+        all = Wallpapers.find({}).fetch();
+        chunks = [];
+        size = 3;
+        while (all.length > 3) {
+            chunks.push({row: all.slice(0, 3)});
+            all = all.slice(3);
+        }
+        chunks.push({row: all});
+        console.log(chunks);
+        return chunks;
+        
+    },
+});
+
+
