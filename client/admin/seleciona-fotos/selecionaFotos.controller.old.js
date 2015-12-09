956a8e026b98f0e9c0766246e6f5803b53639292
Saulo Falcão - Casa <saulofalcao@gmail.com>
2015-12-09 11:50:44 -0300

    pagination da selecao de fotos flickr

---
+++ client/admin/seleciona-fotos/selecionaFotos.controller.old.js
@@ -0,0 +1,11 @@
+//Template.selecionaFotos.helpers({
+//   fotosFlickr: function(){
+//        var fotosFlickr = FotosFlickr.find({});
+//        return fotosFlickr;
+//   }
+//});
+//
+//this.PagesSelecionaFotos = new Meteor.Pagination(FotosFlickr, {
+//    itemTemplate: "selecionaFoto"
+//});
+
